c3c1bca78b90719a64a37b042e93b4713b527d76
corvinusNWH9YB <mate.fenyvesvolgyi@stud.uni-corvinus.hu>
2021-04-27 11:30:39 +0200

    Jelszó titkosítása

--- src/main/java/hu/nwh9yb/loginapp/service/LoginServiceImpl.java
+++ src/main/java/hu/nwh9yb/loginapp/service/LoginServiceImpl.java
@@ -4,6 +4,7 @@ import hu.nwh9yb.loginapp.dao.UserEntity;
 import hu.nwh9yb.loginapp.dao.UserRepository;
 import hu.nwh9yb.loginapp.dto.UserDTO;
 import hu.nwh9yb.loginapp.exception.UserAlreadyExistsException;
+import hu.nwh9yb.loginapp.utils.MD5Encrypter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,16 +22,18 @@ public class LoginServiceImpl  implements ILoginService{
         }
         UserEntity userEntity = new UserEntity();
         userEntity.setUsername(user.getUsername());
-        userEntity.setPassword(user.getPassword());
+        // Jelszó titkosítása
+        userEntity.setPassword(MD5Encrypter.getMD5EncryptedValue(user.getPassword()));
         userEntity.setFullName(user.getFullName());
         userRepository.save(userEntity);
-        return MessageFormat.format("{0} sucessfully registered with username: {1}!",user.getFullName(), user.getUsername());
+        return MessageFormat.format(
+                "{0} sucessfully registered with username: {1}!",user.getFullName(), user.getUsername());
     }
 
     @Override
     public String login(String userName, String password) {
         UserEntity userEntity = userRepository.findUserEntityByUsername(userName);
-        if(userEntity.getPassword().equals(password)){
+        if(userEntity.getPassword().equals(MD5Encrypter.getMD5EncryptedValue(password))){
             return MessageFormat.format("Welcome {0}!", userEntity.getFullName());
         }
         throw new SecurityException("Wrong username or password!");
